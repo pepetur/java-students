19ec8f5412090db52ed9b56facadc00b4bafc3d7
Pepe Tur <pepetur26@gmail.com>
2024-01-11 10:16:34 +0100

    first commit

---
+++ src/main/java/travel/compositor/studentsjava/StudentsJavaApplication.java
@@ -0,0 +1,13 @@
+package travel.compositor.studentsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/travel/compositor/studentsjava/StudentsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package travel.compositor.studentsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
